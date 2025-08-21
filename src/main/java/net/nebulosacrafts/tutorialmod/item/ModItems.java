319e4e581c6ebbfd234c9162bf6c912ac879004e
Lucía <luciapiedrabuena1@gmail.com>
2025-08-22 01:40:13 +0200

    Initial commit

---
+++ src/main/java/net/nebulosacrafts/tutorialmod/item/ModItems.java
@@ -0,0 +1,41 @@
+package net.nebulosacrafts.tutorialmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nebulosacrafts.tutorialmod.TutorialMod;
+import net.nebulosacrafts.tutorialmod.block.custom.FuelItem;
+import net.nebulosacrafts.tutorialmod.item.custom.MetalDetectorItem;
+import net.nebulosacrafts.tutorialmod.item.custom.medic.HealingSyringeItem;
+import net.nebulosacrafts.tutorialmod.item.custom.medic.StrangeSyringeItem;
+import net.nebulosacrafts.tutorialmod.item.custom.medic.SyringeItem;
+
+public class ModItems {
+
+    // Deferred register es una lista de algo, en este caso Item
+    // Cuando añadamos aquí los items se registarán en un momento específico para que Forge los inyecte
+    // Al mod
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BANDAID = ITEMS.register("bandaid", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector", () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+
+    //syringes
+    public static final RegistryObject<Item> HEALING_SYRINGE = ITEMS.register("healing_syringe", () -> new HealingSyringeItem(new Item.Properties().stacksTo(8)));
+    public static final RegistryObject<Item> STRANGE_SYRINGE = ITEMS.register("strange_syringe", () -> new StrangeSyringeItem(new Item.Properties().stacksTo(8)));
+    public static final RegistryObject<Item> SYRINGE = ITEMS.register("syringe", () -> new SyringeItem(new Item.Properties()));
+
+    public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry", () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
+
+    public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone", () -> new FuelItem(new Item.Properties(), 400));
+
+    // Método estático para poder acceder a registrar los elementos en el eventBus que habrá en la clase main
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
