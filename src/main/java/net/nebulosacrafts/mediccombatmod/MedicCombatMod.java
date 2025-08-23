ca4d94715110174b9e7a6c975701c0bd1dbfd326
Lucía <luciapiedrabuena1@gmail.com>
2025-08-23 21:34:22 +0200

    Refactored modId

--- src/main/java/net/nebulosacrafts/mediccombatmod/MedicCombatMod.java
+++ src/main/java/net/nebulosacrafts/mediccombatmod/MedicCombatMod.java
@@ -22,7 +22,7 @@ import org.slf4j.Logger;
 public class MedicCombatMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "medic_combat_mod";
+    public static final String MOD_ID = "mediccombatmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
